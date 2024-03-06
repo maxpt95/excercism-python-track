214974552c882c9fab3105c8ce81b423451772b8
Perez Tambasco Maximiliano TEI-O <Maximiliano.PerezTambasco@axpo.group>
2024-03-06 15:41:06 +0100

    Added docstrings

--- excercises/amstrong_numbers.py
+++ excercises/amstrong_numbers.py
@@ -11,6 +11,11 @@
 
 
 def is_armstrong_number(number: int) -> bool:
+    """Checks if the number given is an armstrong number
+
+    :param number: int - possible armstrong number.
+    :return: bool - true if armstrong number.
+    """
     number_str = str(number)
     return number == sum(int(digit) ** len(number_str) for digit in number_str)
 
