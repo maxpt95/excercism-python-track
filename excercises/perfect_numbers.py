ea8fb462b6993f4e5e417d46d96c70a9c73a85f7
Perez Tambasco Maximiliano TEI-O <Maximiliano.PerezTambasco@axpo.group>
2024-07-31 12:46:08 +0200

    Simplified perfect_numbers excercised. Removed the two functions as they were only one line of code

--- excercises/perfect_numbers.py
+++ excercises/perfect_numbers.py
@@ -1,20 +1,4 @@
 import math
-from typing import Set, Tuple
-
-
-def get_factors(number: int) -> Set[Tuple]:
-    """A perfect number equals the sum of its positive divisors.
-
-    :param number: int a positive integer
-    :return: Set[Tuple] a set containing all factor paris use in the number division
-    """
-    return set((i, number / i) for i in range(1, math.ceil(math.sqrt(number))) if number % i == 0)
-
-
-def aliquot_sum(number: int) -> int:
-    """Performs an aliquot sum on the number given"""
-    factors = get_factors(number)
-    return sum(first + second for first, second in factors) - number
 
 
 def classify(number: int) -> str:
@@ -26,12 +10,14 @@ def classify(number: int) -> str:
     if type(number) is not int or number <= 0:
         raise ValueError("Classification is only possible for positive integers.")
 
-    aliquot = aliquot_sum(number)
-    if number == aliquot:
-        clasification = "perfect"
-    elif number < aliquot:
-        clasification = "abundant"
-    elif number > aliquot:
-        clasification = "deficient"
+    # Get all the factors of a number
+    factors = set((i, number / i) for i in range(1, math.ceil(math.sqrt(number))) if number % i == 0)
+    # The aliquout sum ofa number is the sum of all the factors of the number except for itself
+    aliquot = sum(first + second for first, second in factors) - number
 
-    return clasification
+    if number == aliquot:
+        return "perfect"
+    elif number < aliquot:
+        return "abundant"
+    elif number > aliquot:
+        return "deficient"
